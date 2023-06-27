497d4c1d3b985a96bdb8a506885f162b25eb2c21
Nirajan Karki <nirajankarki1985@gmail.com>
2023-06-27 21:19:03 +0200

    Stats component done

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onUpdateItem={handleUpdateItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -103,10 +103,24 @@ function Item({ item, onDeleteItem, onUpdateItem }) {
     </li>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em> 💼 Start adding items to your packing list !</em>
+      </p>
+    );
+  }
+  const numItems = items.length;
+  const packedItems = items.filter((item) => item.packed === true).length;
+  const percentage = Math.round((packedItems / numItems) * 100);
   return (
     <footer className="stats">
-      <em>💼 You have 6 items on your list, and you already packed 0 (0%)</em>
+      <em>
+        {percentage === 100
+          ? `You have packed everything ! Ready to go ✈🚢 !`
+          : `You have ${numItems} items on your list, and you have packed ${packedItems} items (${percentage}%)`}
+      </em>
     </footer>
   );
 }
